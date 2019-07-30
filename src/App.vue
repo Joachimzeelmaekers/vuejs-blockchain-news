ac383df4c60fe15004dae775406cae7f7ed56273
Joachimzeelmaekers <joa.zeelmaekers@hotmail.com>
2019-07-30 09:39:16 +0200

    added styling

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,103 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Movies"/>
+    <div class="cards-container">
+      <Card v-for="card in cards" :card="card" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import Card from "./components/Card.vue";
+import axios from "axios";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    HelloWorld,
+    Card
+  },
+  data() {
+    return {
+      cards: [{ name: "testy" }]
+    };
+  },
+  methods: {
+    loadData() {}
+  },
+  created() {
+    console.log("hey");
+    axios({
+      method: "GET",
+      url: "http://localhost:3000/movies"
+    }).then(
+      result => {
+        if (!result && !result.data) {
+          this.loading = false;
+          this.result = "Something went wrong, try again please.";
+        }
+        console.log(result);
+        const data = result.data;
+        this.cards = data;
+      },
+      error => {
+        console.error("error");
+      }
+    );
   }
-}
+};
 </script>
 
-<style>
+<style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+body {
+  font-family: "Ubuntu", sans-serif;
+  * {
+    box-sizing: border-box;
+  }
+}
+.cards-container {
+  display: grid;
+
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: flex-start;
+}
+
+.card-container {
+  flex-basis: 25%;
+  -ms-flex: auto;
+  width: 100%;
+  position: relative;
+  padding: 10px;
+  box-sizing: border-box;
+}
+
+@media(max-width: 1333px) {
+  .card-container {
+    flex-basis: 33.33%;
+  }
+}
+@media(max-width: 1073px) {
+   .card-container {
+    flex-basis: 33.33%;
+  }
+}
+@media(max-width: 815px) {
+  .card-container {
+    flex-basis: 50%;
+  }
+}
+@media(max-width: 555px) {
+  .card-container {
+    flex-basis: 100%;
+  }
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="card-container">
+    <div class="card">
+      <a href="hottub">
+        <div class="card--display">
+          <img v-bind:src="card.img"/>
+          <h2>{{card.title}}</h2>
+        </div>
+        <div class="card--hover">
+          <h2>{{card.title}}</h2>
+					<p>{{card.year}}</p>
+          <p>{{card.description}}</p>
+          <p class="link">Click to see the actors</p>
+        </div>
+      </a>
+      <div class="card--border"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: {
+    card:Object
+  }
+};
+</script>
+
+<style scoped lang="scss">
+@import "../styles/cards";
+</style>
